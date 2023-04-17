865e8f5841aab023182bad3fa4d59f888891563b
DenverLacey <36467790+DenverLacey@users.noreply.github.com>
2023-04-17 12:18:30 +1000

    Change build process to use premake.

---
+++ premake5.lua
@@ -0,0 +1,23 @@
+workspace "re2d"
+    configurations { "debug", "release" }
+
+project "re2d"
+    kind "ConsoleApp"
+    language "C"
+    cdialect "C99"
+
+    files { "src/*.c" }
+
+    includedirs {
+        "src",
+        "/opt/homebrew/Cellar/raylib/4.5.0/include"
+    }
+
+    libdirs {
+        "/opt/homebrew/Cellar/raylib/4.5.0/lib"
+    }
+
+    links { "raylib" }
+
+    filter "configurations:debug"
+        defines { "DEBUG" }
